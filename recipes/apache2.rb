46d987d506a7c0fc17fd8a14ab4c37e262f7e53b
Yoshihisa Amakata <amakata@flatz.jp>
2016-01-29 12:10:31 +0900

    Update apache2.rb

--- recipes/apache2.rb
+++ recipes/apache2.rb
@@ -4,7 +4,6 @@ web_app "phpmyadmin" do
     template 'apache2/web_app.conf.erb'
     server_name node[:phpmyadmin][:apache2][:server_name]
     server_port node[:phpmyadmin][:apache2][:port]
-    server_aliases [node['fqdn']]
     docroot node[:phpmyadmin][:apache2][:docroot]
     allow_ip_addresses node[:phpmyadmin][:apache2][:allow_ip_addresses]
 end
